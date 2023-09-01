ef7ad76402c7dbb436f415843bf5f8f812f441aa
matovu-farid <matovu90@gmail.com>
2023-09-01 13:15:56 +0300

    Generate opening brackets

--- main.cpp
+++ main.cpp
@@ -1,74 +1,26 @@
-#include <cstdio>
 #ifndef ONLINE_JUDGE
 #include "store/print.h"
 #endif
 
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 
 using namespace std;
-
-const int N = 100;
-int board[N][N];
-const int QUEEN = 1;
-const int BLANK = 0;
-
-void printBoard(int n) {
-  unordered_map<int, string> map = {{1 , "👑"} ,{0 , "🟩"}};
-
-  for (int r = 0; r < n; r++) {
-    for (int c = 0; c < n; c++) {
-      cout << map[board[r][c]] << " ";
-    }
-    cout << endl;
+void generate_brackets(int closing, int opening, string current = ""){
+  if (closing == 0 and opening == 0 ){
+    cout << current << endl;
+    return;
   }
-  cout << endl;
-}
-bool canPlace(int r, int c, int n) {
-  for (int i = 0; i < n; i++)
-    if (board[r][i] == QUEEN )
-      return false;
+  if (opening < 0 || opening > closing) return;
+  generate_brackets(closing - 1, opening, current + ')');
+  generate_brackets(closing, opening - 1, current + '(');
 
-  for (int i = r, j = c; i >= 0 && j >= 0; i--, j--)
-    if (board[i][j] == QUEEN)
-      return false;
-
-  for (int i = r, j = c; i < n && j >= 0; i++, j--)
-    if (board[i][j] == QUEEN)
-      return false;
-
-  return true;
-}
-void getAllValidConfigs(int col, int n,int* count, int limit=4) {
-  if (*count >= limit) return;
-  if (col == n) {
-    printBoard(n);
-    (*count) += 1;
-    return ;
-  }
-  for (int row = 0; row < n; row++) {
-    if (!canPlace(row, col, n))
-      continue;
-
-    board[row][col] = QUEEN;
-    getAllValidConfigs(col + 1, n,count, limit);
-    board[row][col] = BLANK;
-  }
 }
 
 void solve() {
   int n;
   cin >> n;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      board[i][j] = BLANK;
-    }
-  }
-  int count = 0;
-  int *countPointer = &count;
-  getAllValidConfigs(0, n, countPointer, 5);
-  cout << endl;
+  generate_brackets(n, n);
 }
 
 int main() {
