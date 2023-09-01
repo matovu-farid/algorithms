48c9815fe2219f5cebc3e6b37dffd05898cc971a
matovu-farid <matovu90@gmail.com>
2023-09-01 19:46:21 +0300

    Solve sudoku board

--- main.cpp
+++ main.cpp
@@ -6,21 +6,65 @@
 #include <vector>
 
 using namespace std;
-void generate_brackets(int closing, int opening, string current = ""){
-  if (closing == 0 and opening == 0 ){
-    cout << current << endl;
-    return;
-  }
-  if (opening < 0 || opening > closing) return;
-  generate_brackets(closing - 1, opening, current + ')');
-  generate_brackets(closing, opening - 1, current + '(');
+const int N = 9;
+int board[N][N];
 
+void printBoard() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << board[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+bool canPlace(int row, int col, int num) {
+  for (int i = 0; i < N; i++)
+    if (board[i][col] == num || board[row][i] == num)
+      return false;
+  int rowIdx = row / 3;
+  int colIdx = col / 3;
+  int startRow = rowIdx * 3;
+  int startCol = colIdx * 3;
+  for (int i = startRow; i < startRow + 3; i++)
+    for (int j = startCol; j < startCol + 3; j++)
+      if (board[i][j] == num)
+        return false;
+  return true;
+}
+bool solveSudoku(int i, int j) {
+  if (i == N) {
+    printBoard();
+    return true;
+  }
+  int next_col = j + 1;
+  int next_row = i;
+  if (next_col == N) {
+    next_col = 0;
+    next_row += 1;
+  }
+
+  if (board[i][j]) {
+    return solveSudoku(next_row, next_col);
+  }
+  for (int num = 1; num <= N; num++) {
+    if (!canPlace(i, j, num))
+      continue;
+    board[i][j] = num;
+    if (solveSudoku(next_row, next_col))
+      return true;
+    board[i][j] = 0;
+  }
+  return false;
 }
 
 void solve() {
-  int n;
-  cin >> n;
-  generate_brackets(n, n);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> board[i][j];
+    }
+  }
+  solveSudoku(0, 0);
 }
 
 int main() {
