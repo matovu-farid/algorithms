c4724de7fa3f074394e9728dfbd8d746db5049dd
matovu-farid <matovu90@gmail.com>
2023-09-04 18:06:14 +0300

    Add fib tabulation

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 using ll = long long;
 
-long long fib(ll n, vector<ll> &memo) {
+ ll fib(int n, vector<ll> &memo) {
   if (memo[n] != -1)
     return memo[n];
   if (n <= 2)
@@ -17,13 +17,20 @@ long long fib(ll n, vector<ll> &memo) {
   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
   return memo[n];
 }
+ll fib(int n){
+  ll dp[n + 1];
+  dp[1] = dp[2] = 1;
+  for (int i = 3; i <= n; i++)
+    dp[i] = dp[i - 2] + dp[i - 1];
+  return dp[n];
+}
+
 using namespace std;
 
 void solve() {
   int n;
   cin >> n;
-  vector<ll> memo(n + 1, -1);
-  cout << (long double)fib(n, memo) << endl;
+  cout << (long double)fib(n) << endl;
 }
 
 int main() {
