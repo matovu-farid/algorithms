3648012c2d4dc8c978521685f04c3ee936a2a260
matovu-farid <matovu90@gmail.com>
2023-09-06 01:08:12 +0300

    Solve alphacodee problem

--- main.cpp
+++ main.cpp
@@ -7,19 +7,23 @@
 using ll = long long;
 
 using namespace std;
-
-
-void solve() {
-  int n;
-  cin >> n;
-  int dp[n + 1];
-  dp[1]= 0;
-  for (int i = 2; i <= n; i++){
-    dp[i] = 1 + dp[i - 1];
-    if (i % 2 == 0)
-      dp[i] = min(dp[i],1 + dp[i / 2]);
-    if (i % 3 == 0)
-      dp[i] = min(dp[i],1 + dp[i / 3]);
+// https://www.spoj.com/problems/ACODE/
+void solve(string s) {
+  int n = s.length();
+  ll dp[n + 1];
+  dp[0] = 1;
+  dp[1] = 1;
+  int charCode = stoi(s.substr(0, 2));
+  if (charCode <= 26 && charCode >= 11 && charCode != 20)
+    dp[1] += 1;
+  for (int i = 2; i <= n; i++) {
+    dp[i] = 0;
+    if (s[i] != '0' )
+      dp[i] += dp[i - 1];
+    int charCode = stoi(s.substr(i - 1, 2));
+    if ( charCode <= 26 && charCode >= 10) {
+      dp[i] += dp[i - 2];
+    }
   }
   cout << dp[n] << endl;
 }
@@ -31,11 +35,12 @@ int main() {
 #endif
   ios::sync_with_stdio(0);
   cin.tie(0);
+  string s;
 
-  int tests;
-  cin >> tests;
-  while (tests--) {
-    solve();
+  cin >> s;
+  while (s != "0") {
+    solve(s);
+    cin >> s;
   }
 
   return 0;
