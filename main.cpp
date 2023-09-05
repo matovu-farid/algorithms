5046dbad1d6facb748581fad55191bedc1c7da96
matovu-farid <matovu90@gmail.com>
2023-09-05 03:44:27 +0300

    TD- fill array without consequtive zeroes

--- main.cpp
+++ main.cpp
@@ -1,36 +1,26 @@
-#include <climits>
-#include <ios>
+#include <algorithm>
 #ifndef ONLINE_JUDGE
 #include "store/print.h"
 #endif
 
-#include <iomanip>
 #include <iostream>
 #include <vector>
 using ll = long long;
-
- ll fib(int n, vector<ll> &memo) {
-  if (memo[n] != -1)
-    return memo[n];
-  if (n <= 2)
-    return 1;
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
-  return memo[n];
-}
-ll fib(int n){
-  ll dp[n + 1];
-  dp[1] = dp[2] = 1;
-  for (int i = 3; i <= n; i++)
-    dp[i] = dp[i - 2] + dp[i - 1];
-  return dp[n];
-}
-
 using namespace std;
 
+// No of ways to fill an array of length n with only zeros and ones without consequtive ones.
+int ways(int n, vector<int>& memo){
+  if (n == 0) return 1;
+  if (n == 1) return 2;
+  if (memo[n] != -1) return memo[n];
+  memo[n] =  ways(n - 1, memo) + ways(n - 2, memo);
+  return memo[n];
+}
 void solve() {
   int n;
   cin >> n;
-  cout << (long double)fib(n) << endl;
+  vector<int> memo(n + 1, -1);
+  cout << ways(n, memo) << endl;
 }
 
 int main() {
@@ -38,8 +28,6 @@ int main() {
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
 #endif
-  cout.precision(3);
-
   ios::sync_with_stdio(0);
   cin.tie(0);
 
