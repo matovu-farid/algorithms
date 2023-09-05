bbd109e603ce50afe34a450c072a89545befdfbc
matovu-farid <matovu90@gmail.com>
2023-09-05 23:25:43 +0300

    Solve ways to reduce number by n - 1, n / 2 and n / 3

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #ifndef ONLINE_JUDGE
 #include "store/print.h"
 #endif
@@ -9,32 +8,20 @@ using ll = long long;
 
 using namespace std;
 
-int max_money(int activities[], int n) {
-  int dp[n];
-  int max_prev[n];
-  dp[0] = max_prev[0] = activities[0];
-  dp[1]  = max_prev[1]= activities[1] + activities[0];
-
-  dp[2] = activities[2] + max(activities[1], activities[0]);
-  max_prev[2] = max(dp[2], dp[1]);
-
-
-  for (int i = 3; i < n; i++){
-    
-
-    dp[i] = activities[i] + max({dp[i - 2], max_prev[i - 3] + activities[i - 1]});
-    max_prev[i] = max(max_prev[i - 1], dp[i]);
-  }
-  return max({dp[n - 2], dp[n - 1]});
-}
 
 void solve() {
   int n;
   cin >> n;
-  int activities[n];
-  for (int i = 0; i < n; i++)
-    cin >> activities[i];
-  cout << max_money(activities, n) << endl;
+  int dp[n + 1];
+  dp[1]= 0;
+  for (int i = 2; i <= n; i++){
+    dp[i] = 1 + dp[i - 1];
+    if (i % 2 == 0)
+      dp[i] = min(dp[i],1 + dp[i / 2]);
+    if (i % 3 == 0)
+      dp[i] = min(dp[i],1 + dp[i / 3]);
+  }
+  cout << dp[n] << endl;
 }
 
 int main() {
