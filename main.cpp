ad019c9739806e867c971f006b5e71daa8f94be3
matovu-farid <matovu90@gmail.com>
2023-09-05 03:59:26 +0300

    Number of set bits in a number

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #ifndef ONLINE_JUDGE
 #include "store/print.h"
 #endif
@@ -6,30 +5,18 @@
 #include <iostream>
 #include <vector>
 using ll = long long;
+
 using namespace std;
 
-// No of ways to fill an array of length n with only zeros and ones without consequtive ones.
-int ways(int n, vector<int>& memo){
-  if (n == 0) return 1;
-  if (n == 1) return 2;
-  if (memo[n] != -1) return memo[n];
-  memo[n] =  ways(n - 1, memo) + ways(n - 2, memo);
-  return memo[n];
-}
-int ways(int n){
-  int dp[n + 1];
-  fill(dp, dp + n, 0);
-  dp[0] = 1;
-  dp[1] = 2;
-  for (int i = 2; i <= n; i++)
-    dp[i] = dp[i - 1] + dp[i - 2];
-  return dp[n];
-}
 void solve() {
   int n;
   cin >> n;
-  vector<int> memo(n + 1, -1);
-  cout << ways(n) << endl;
+  int count = 0;
+  while (n){
+    if (n & 1) count++;
+    n >>= 1;
+  }
+  cout << count << endl;
 }
 
 int main() {
