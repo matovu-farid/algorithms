b9ad640def2e98b7a50a4b79532872885f2bae45
matovu-farid <matovu90@gmail.com>
2023-09-07 02:33:18 +0300

    math: Solve gcd

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <unordered_set>
 #ifndef ONLINE_JUDGE
 #include "store/print.h"
 #endif
@@ -6,41 +7,19 @@
 #include <vector>
 using ll = long long;
 
-// https://codeforces.com/problemset/problem/1091/D
 using namespace std;
-vector<ll> dp(1e5, 0);
 
-
-const int MOD = 998244353;
-long long fact(int n){
-  if(dp[n]) return dp[n];
-
-  if (n <= 1) return 1;
-  dp[n] =  (n * fact(n - 1)) % MOD;
-  return dp[n];
-
+int gcd(int n, int m){
+  if (m == 0) return n;
+  return gcd(m, n % m);
 }
-ll power(int n, int p){
-  if(p == 0) return 1;
-  if(p == 1) return n;
-  if (p & 1) return (n * power(n , p - 1)) % MOD;
-  ll temp  = (power(n, p / 2)) % MOD;
-  return (temp * temp) % MOD;
 
-}
 void solve() {
-  int n;
-  cin >> n;
-  dp.resize(n + 1, 0);
-  ll total = (n * fact(n)) % MOD ;
-  ll decreasingSuffixes = 0;
-  for(int k = 1; k < n; k ++){
-    decreasingSuffixes += ((fact(n) * power(fact(k), MOD - 2))) % MOD ;
-
-  }
-  decreasingSuffixes %= MOD;
-  ll res = (total - decreasingSuffixes + MOD) % MOD;
-  cout << res << endl;
+  int n, m;
+  cin >> n >> m;
+  if (m > n) swap(n, m);
+  cout << gcd(n, m) << endl;
+  
 }
 
 int main() {
@@ -51,7 +30,11 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-
+  int tests;
+  cin >> tests;
+  while (tests--) {
     solve();
+  }
+
   return 0;
 }
