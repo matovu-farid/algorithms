adfb55852b4114b22d530f4a62999f5b8169d742
matovu-farid <matovu90@gmail.com>
2023-09-07 04:58:22 +0300

    math: extended euclid with gcd returned

--- main.cpp
+++ main.cpp
@@ -10,22 +10,22 @@ using ll = long long;
 using namespace std;
 
 // find values or x and y such that, ax + by = gcd(a, b)
-vector<int> extended_gcd(int a, int b){
-  if (b == 0) 
-    return {1, 0};
+vector<int> extended_gcd(int a, int b) {
+  if (b == 0)
+    return {1, 0, a};
   vector<int> res = extended_gcd(b, a % b);
   int x = res[1];
-  int y =res[0] - (a / b) * x;
-  return {x, y};
+  int y = res[0] - (a / b) * x;
+  return {x, y, res[2]};
 }
 
 void solve() {
   int a, b;
   cin >> a >> b;
-  if (b > a) swap(a, b);
-  auto res  = extended_gcd(a, b);
-  cout << res[0] << " " << res[1] << endl;
-  
+  if (b > a)
+    swap(a, b);
+  auto res = extended_gcd(a, b);
+  cout << res[0] << " " << res[1] << " " << res[2] << endl;
 }
 
 int main() {
