36d79d26f70ca4dd948c3d939237c3d72e55727e
matovu-farid <matovu90@gmail.com>
2023-09-20 20:09:52 +0300

    bits: longest consecutive run of 1's

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #ifndef ONLINE_JUDGE
 #include "store/print.h"
 #endif
@@ -7,46 +6,29 @@
 #include <vector>
 using ll = long long;
 
-/*
- Sort Integers by The Number of 1 Bits
-Given an integer array arr. You have to sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
-
-Return the sorted array.
-*/
 using namespace std;
-int count_bits(int n) {
-  int count = 0;
-  while (n) {
-    n = n & (n - 1);
-    count++;
-  }
-  return count;
-}
-vector<int> sortByBits(vector<int> arr) {
-  int n = arr.size();
-  vector<int> res;
-  vector<vector<int>> buckets(30);
-  for (int num : arr) {
-    int bit_count = count_bits(num);
-    buckets[bit_count].push_back(num);
-  }
-  for (auto bucket : buckets) {
-    sort(bucket.begin(), bucket.end());
-    for (int num : bucket)
-      res.push_back(num);
-  }
-  return res;
-}
-
+/*
+Longest Consecutive Run of 1s in Binary
+Given a non-negative integer n, return the length of the longest consecutive run
+of 1s in its binary representation.
+*/
 void solve() {
   int n;
   cin >> n;
-  vector<int> arr(n);
-  for (int i = 0; i < n; i++) {
-    cin >> arr[i];
+  int max_count = 0;
+  int current_count = 0;
+  while (n) {
+    if (n & 1) {
+      current_count++;
+    } else {
+      max_count = max(max_count, current_count);
+      current_count = 0;
+    }
+    n >>= 1;
   }
-  vector<int> sorted_array = sortByBits(arr);
-  print(sorted_array);
+  max_count = max(max_count, current_count);
+
+  cout << max_count << endl;
 }
 
 int main() {
