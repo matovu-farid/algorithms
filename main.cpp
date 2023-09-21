71db14b83b6a30babcdd6aae6b81eb92dd5566c0
matovu-farid <matovu90@gmail.com>
2023-09-21 07:40:19 +0300

    bits: total hamming distance - more efficient approach

--- main.cpp
+++ main.cpp
@@ -13,24 +13,16 @@ The Hamming distance between two integers is the number of positions at which th
 
 Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
 */
-int count_bits(int n) {
-  int count = 0;
-  while (n) {
-    n = n & (n - 1);
-    count++;
-  }
-  return count;
-}
+
 int totalHammingDistance(vector<int> nums) {
   int res = 0;
   int n = nums.size();
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i + 1; j < n; j++) {
-      int diff = (nums[i] ^ nums[j]);
-      int distance = count_bits(diff);
-
-      res += distance;
+  for (int i = 0; i < 32; i++){
+    int count = 0;
+    for(int num: nums){
+      if(num & (1 << i)) count++;
     }
+    res += count * (n - count);
   }
   return res;
 }
