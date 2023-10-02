e6dc1f871a0a8ab150b4427d9482a14eec16f34c
matovu-farid <matovu90@gmail.com>
2023-10-02 16:51:34 +0300

    dp: knapsack - space optimized

--- main.cpp
+++ main.cpp
@@ -9,20 +9,16 @@ using ll = long long;
 
 using namespace std;
 
-int knapsack(vector<pair<int, int>> items, int N, int S){
-  vector<vector<int>> dp(N+1, vector<int>(S + 1, 0));
-  for (int i = 1; i <= N; i++ ){
-    for (int j = 0; j <= S; j++){
-      dp[i][j] = dp[i - 1][j];
+int knapsack(vector<pair<int, int>> items, int N, int S) {
+  vector<int> dp(S + 1, 0);
+  for (int i = 1; i <= N; i++) {
+    for (int j = S; j >= 0; j--) {
       auto item = items[i - 1];
-      if(j - item.first >= 0){
-       
-        dp[i][j] = max(dp[i][j], dp[i - 1][j - item.first] + item.second);
-      }
-
+      if (j - item.first >= 0)
+        dp[j] = max(dp[j], dp[j - item.first] + item.second);
     }
   }
-  return dp[N][S];
+  return dp[S];
 }
 
 void solve() {
@@ -32,13 +28,9 @@ void solve() {
   for (int i = 0; i < N; i++) {
     cin >> items[i].first >> items[i].second;
   }
-  cout << knapsack(items, N,  S) << endl;
-  
-  // for(auto p: items) {
-  //   print(p);
-  //   cout << endl;
-  // }
-  
+  cout << knapsack(items, N, S) << endl;
+
+ 
 }
 
 int main() {
